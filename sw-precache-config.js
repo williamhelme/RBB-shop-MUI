3452e17c7037684372be128a12111ab76bd0ebff
William Helme <helme360@gmail.com>
2019-02-04 12:55:18 +0000

    removing server-worker-custom as the script does not exist

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,6 @@ module.exports = {
   staticFileGlobs: ["build/static/css/**.css", "build/static/js/**.js"],
   swFilePath: "./build/service-worker.js",
   stripPrefix: "build/",
-  navigateFallbackWhitelist: [/^(?!\/__)/, ' /getProjectConfig/'],
-  importScripts: ["./service-worker-custom.js"],
+  navigateFallbackWhitelist: [/^(?!\/__)/, " /getProjectConfig/"],
   handleFetch: false
 };
