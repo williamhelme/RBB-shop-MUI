dc5d8d0e24dfb39ab93b3ed05080068c3da2e22b
William Helme <helme360@gmail.com>
2019-02-04 12:07:14 +0000

    Updated the body to use the new product list and login form components

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route } from "react-router";
-import Grid from "@material-ui/core/Grid";
-import Card from "./Components/Card/";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import InsertForm from "./Components/InsertForm";
+import ProductList from "./Components/ProductList";
+import LoginForm from "./Components/LoginForm";
 
 class Body extends Component {
   constructor(props) {
@@ -17,44 +16,46 @@ class Body extends Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.items) {
       this.setState({
-        items: nextProps.items
+        items: nextProps.items,
+        loading: false
       });
     }
   }
 
   render() {
-    const { loading = true } = this.props;
+    const { items = [], loading = true } = this.state;
     return (
       <main>
         <h1>Welcome to Bex's Bakes</h1>
         <Router>
-          <Grid container spacing={24} className={"containerGrid"}>
+          <div>
+            <Route
+              path="/insert"
+              exact
+              component={() => {
+                return (
+                  <div
+                    style={{
+                      textAlign: "right",
+                      top: "-85px",
+                      position: "relative",
+                      marginRight: "10px"
+                    }}
+                  >
+                    <LoginForm />
+                  </div>
+                );
+              }}
+            />
             <Route
               path="/"
               exact
               component={() => (
-                <div>
-                  {loading && (
-                    <Grid item xs={12} sm={12} md={12} lg={12}>
-                      <CircularProgress size={50} />
-                    </Grid>
-                  )}
-                  {this.state.items.map(
-                    ({ title, description, image, name }, key) => (
-                      <Grid item xs={12} sm={12} md={6} lg={3} key={key}>
-                        <Card
-                          title={name}
-                          description={description}
-                          image={image}
-                        />
-                      </Grid>
-                    )
-                  )}
-                </div>
+                <ProductList isLoading={loading} items={items} />
               )}
             />
             <Route path="/insert" exact component={InsertForm} />
-          </Grid>
+          </div>
         </Router>
       </main>
     );
